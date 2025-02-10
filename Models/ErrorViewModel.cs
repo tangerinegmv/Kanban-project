65d1699443664ec0f18921e61f0af1013a176d17
Victoria <gimenezmariavictoria95@gmail.com>
2025-02-10 17:44:21 -0300

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tl2_proyecto_2024_tangerinegmv.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
